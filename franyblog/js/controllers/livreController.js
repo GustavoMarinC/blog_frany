2362441e54c09aa2405c13d14c738df9caa80bf5
GustavoMarinC <gustavo.almaca@gmail.com>
2020-04-09 11:29:19 -0500

    comentarios

--- franyblog/js/controllers/livreController.js
+++ franyblog/js/controllers/livreController.js
@@ -1,3 +1,31 @@
-app.controller('LivreController',['scope',function($scope){
-    $scope.title='LIVRE!!!'
+app.controller('LivreController',['$scope',function($scope){
+    $scope.title='LIVRE!!!';
+    $scope.comentar=false;
+    $scope.comentarios={
+        status:'success',
+        data: [
+            {
+                id:1,
+                usuario:'Usuario1',
+                titulo:'Titulo1',
+                comentario:'Esto es un comentario de prueba de texto'
+            },
+            {
+                id:2,
+                usuario:'Usuario2',
+                titulo:'Titulo2',
+                comentario:'Esto es un comentario de prueba de texto'
+            },
+            {
+                id:3,
+                usuario:'Usuario3',
+                titulo:'Titulo3',
+                comentario:'Esto es un comentario de prueba de texto'
+            }
+        ]
+    };
+
+    $scope.formularioComentar=()=>{
+        $scope.comentar=true;
+    }
 }]);
