4887e14f949684328df09c41fc30e2353d756873
GustavoMarinC <gustavo.almaca@gmail.com>
2020-05-24 13:58:47 -0500

    features-2020 mayo 24

--- franyblog/js/controllers/livreController.js
+++ franyblog/js/controllers/livreController.js
@@ -1,31 +1,37 @@
-app.controller('LivreController',['$scope',function($scope){
+app.controller('LivreController',['$scope','peticionesFactory','$window',async ($scope,peticionesFactory,$window)=>{
+    let data = await peticionesFactory.obtenerComentarios();
     $scope.title='LIVRE!!!';
     $scope.comentar=false;
-    $scope.comentarios={
-        status:'success',
-        data: [
-            {
-                id:1,
-                usuario:'Usuario1',
-                titulo:'Titulo1',
-                comentario:'Esto es un comentario de prueba de texto'
-            },
-            {
-                id:2,
-                usuario:'Usuario2',
-                titulo:'Titulo2',
-                comentario:'Esto es un comentario de prueba de texto'
-            },
-            {
-                id:3,
-                usuario:'Usuario3',
-                titulo:'Titulo3',
-                comentario:'Esto es un comentario de prueba de texto'
-            }
-        ]
-    };
+    $scope.comentario=data;
+    $scope.usuario='';
+    $scope.titulo='';
+    $scope.comentarioNuevo='';
 
+    console.log('comentarios',$scope.comentario);
+    $scope.addComentario;
+    
+    
+    $scope.addComentario =  ()=>{
+
+        console.log('flag1',$scope.usuario,$scope.titulo,$scope.comentarioNuevo)
+         peticionesFactory.guardarComentario(
+            {
+                usuario:$scope.usuario,
+                titulo:$scope.titulo,
+                comentario:$scope.comentarioNuevo
+            },
+            ()=>{
+                $window.location.reload();
+            }
+        );
+      
+
+    }
     $scope.formularioComentar=()=>{
         $scope.comentar=true;
+        $scope.usuario='';
+        $scope.titulo='';
+        $scope.comentarioNuevo='';
     }
+    $scope.$digest();
 }]);
--- franyblog/js/factorias/servicios.js
+++ franyblog/js/factorias/servicios.js
@@ -18,6 +18,41 @@ app.factory('peticionesFactory', function($http){
                     })
 
             });
+          },
+
+          obtenerComentarios: function(){
+            return  new Promise((resolve, reject) => {
+
+                $http({
+                    method : "GET",  
+                    url: url+'comentarios'})
+                    .then(async (res)=>{ 
+                            var comentarios  = res.data.data;        
+                            //  return await  fotos;
+                            resolve(comentarios);
+                    },function(error){
+                        console.log(error);
+                        reject(error);
+                    })
+
+            });
+          },
+
+          guardarComentario: function(json,successCallback){
+              console.log(json)
+              /*
+              var jsontest={
+                usuario:"prueba usuario",
+                titulo:"prueba web titulo",
+                comentario:"prueba web comentario"
+              }
+              */
+              var params='json='+JSON.stringify(json)
+              $http.post(url+'add_comentario',params,{headers:{'Content-Type':'application/x-www-form-urlencoded'}}
+             
+              ).then(
+                  successCallback()
+                  )
           }
     }
     return peticionesFactory;
